75d8e99a5d7757ea09d7dc041ef26b9130d4038b
dominyxu <dominyxu@outlook.com>
2026-01-29 16:35:31 -0500

    dsp fncs

--- c_model/src/dsp.cpp
+++ c_model/src/dsp.cpp
@@ -6,10 +6,30 @@
 #include <cmath>
 #include <iostream>
 
-
 #include "dsp.h"
 
 // Add DSP functions here
+// y-> ouput x-> input h->impulse
+void fir_block_processing(std::vector<float>& y, const std::vector<float>& x, const std::vector<float>& h, std::vector<float>& state)
+{
+    y.clear();
+    y.resize(x.size(), 0.0);
+
+    //convolution
+    for (int i = 0; i < x.size(); ++i) {
+        for (int j = 0; j < h.size(); ++j) {
+            if (i-j >= 0){
+                y[i] = h[j] * x[i-j];
+            }
+            else {
+                y[i] = h[j] * state[(i-j) + (h.size() - 1)];
+            }
+        }
+    }
+
+    // update state: keep last (hsize-1) samples of x
+    for (int i = 0; i < h.size() - 1; ++i)
+        state[i] = x[x.size() - (h.size() - 1) + i];
+}
 
 
-// block processing functions 
