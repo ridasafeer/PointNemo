51b26fd2f7bce92e10bf5fa039c5f55c1a559353
Samarth Patel <113307694+samarthp3@users.noreply.github.com>
2026-02-02 20:03:06 -0500

    weeeeee secondary path code done

--- c_model/include/controller.h
+++ c_model/include/controller.h
@@ -9,7 +9,7 @@ public:
     Controller(std::vector<float> shat, int L, float mu);
 
     //test for estimated secondary path via prbs
-    std::vector<float> calibration();
+    std::vector<float> calibration(const std::vector<float>& x_exc, const std::vector<float>& y_mic, int L, float mu, int passes = 1, float leak = 0.0f);
 
    //Starts learning loop
     void startLearningLoop(float* referenceSignal, float* desiredSignal, int signalLength);
