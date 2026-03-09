256312f818d2ad60bc3536c6dc96f3a9385efa14
ridasafeer <safeerr@mcmaster.ca>
2026-03-09 07:12:52 -0400

    controller constructor fix in .h

--- c_model/include/controller.h
+++ c_model/include/controller.h
@@ -9,7 +9,7 @@
 class Controller {
 public:
     // Constructor: creates FxLMS & DSP objects
-    Controller(std::vector<float> &x, std::vector<float> &y, std::vector<float> shat, int L, float mu);
+    Controller(std::vector<float> shat, int L, float mu);
 
     //test for estimated secondary path via prbs
     std::vector<float> calibration(const std::vector<float>& x_exc, const std::vector<float>& y_mic, int L, float mu, int passes = 1, float leak = 0.0f);
