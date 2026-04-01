d4e94db713af9a3896048b0ebaf1630d8d071421
Dominy Xu <xud48@mcmaster.com>
2026-04-01 06:38:25 -0400

    output() update() done

--- c_model/src/fxlms.cpp
+++ c_model/src/fxlms.cpp
@@ -6,13 +6,36 @@
 
 // Constructor
 FxLMS::FxLMS(const std::vector<float>& shat, int L, float mu)
-    : L(L), M(static_cast<int>(shat.size())), mu(mu), shat(shat), w(L, 0.0f), x(*(new std::vector<float>(L, 0.0f))), xf(L, 0.0f) {
+    : L(L), 
+    M(static_cast<int>(shat.size())), 
+    mu(mu), 
+    shat(shat), 
+    head(0),
+
+    shat(shat),
+    w(L, 0.0f), 
+    x(*(new std::vector<float>(L, 0.0f))), 
+    xf(L, 0.0f),
+    y(1, 0.0f), {
     std::cout << "FxLMS constructor" << std::endl;
 }
 
-void FxLMS::output(int startIndex) const {
-    //produce the anti-noise signal y(n), propagate it forward for speaker output (will travel through S(z) physically)
-    //Convolution of filter coefficients with reference signal
+//-----------------------------
+// output
+// Computes y(n) = w^T . x_circular
+ 
+//produce the anti-noise signal y(n), propagate it forward for speaker output (will travel through S(z) physically)
+//Convolution of filter coefficients with reference signal
+
+void FxLMS::output(int startIndex) {
+    head = startIndex;
+
+    //takes index of ciruclar buffer to lenght of adpative filter (# of coefficients)
+    for (int i=0; i<L; i++) {
+        int index = (head - i + L) % L;
+        yn_val += w[k] * x[index]; //compute antinoise
+    }
+    y[0] = yn_val;
     std::cout << "FxLMS.cpp: push_xf()" << std::endl;
     //sliding window logic 2: on the reading for computing each convolution product side
 }
@@ -28,10 +51,25 @@ void FxLMS::push_xf() {
 
 }
 
+//-----------------------------
+// update w coeffs
+// slightly adjust those weights for the next antinoise output
 
-void FxLMS::update(){
+//e_n - error signal
+//w[i] - coeffs of adaptive filter
+//mu - learning rate
+//xf - reference signal post transfer function
+
+void FxLMS::update(float e_n){
     std::cout << "FxLMS.cpp: update()" << std::endl;
 
+    const float mu_e = mu * e_n;   // scalar: pre-multiply once outside the loop
+
+    for (int i = 0; k < L; i++) {
+        w[i] += mu_e * xf[i];  // standard FxLMS 
+        // w[k] = nu * w[k] + mu_e * xf[k];   // leaky FxLMS  idk chat gave me this
+
+    }
 }
 
 //getters for the bindings of the FxLMS x and y buffers to references inside other classes, when in the constructor initializer list
